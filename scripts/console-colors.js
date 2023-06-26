35814faf59a91214c70b924538981ffe87da4251
mohanadft <mohanadfteha@gmail.com>
2023-06-26 17:59:05 +0300

    remove node_modules

--- scripts/console-colors.js
+++ scripts/console-colors.js
@@ -1,57 +1,57 @@
 const colors = {
-  Reset: "\x1b[0m",
-  Bright: "\x1b[1m",
-  Dim: "\x1b[2m",
-  Underscore: "\x1b[4m",
-  Bold: "\x1b[1m",
-  StrikeThrough: "\x1b[9m",
-  Italic: "\x1b[3m",
-  Blink: "\x1b[5m",
-  Reverse: "\x1b[7m",
-  Hidden: "\x1b[8m",
+	Reset: '\x1b[0m',
+	Bright: '\x1b[1m',
+	Dim: '\x1b[2m',
+	Underscore: '\x1b[4m',
+	Bold: '\x1b[1m',
+	StrikeThrough: '\x1b[9m',
+	Italic: '\x1b[3m',
+	Blink: '\x1b[5m',
+	Reverse: '\x1b[7m',
+	Hidden: '\x1b[8m',
 
-  Framed: "\x1b[51m",
-  Encircled: "\x1b[52m",
+	Framed: '\x1b[51m',
+	Encircled: '\x1b[52m',
 
-  Black: "\x1b[30m",
-  Red: "\x1b[31m",
-  Green: "\x1b[32m",
-  Yellow: "\x1b[33m",
-  Blue: "\x1b[34m",
-  Magenta: "\x1b[35m",
-  Cyan: "\x1b[36m",
-  White: "\x1b[37m",
+	Black: '\x1b[30m',
+	Red: '\x1b[31m',
+	Green: '\x1b[32m',
+	Yellow: '\x1b[33m',
+	Blue: '\x1b[34m',
+	Magenta: '\x1b[35m',
+	Cyan: '\x1b[36m',
+	White: '\x1b[37m',
 
-  "Bg-Black": "\x1b[40m",
-  "Bg-Red": "\x1b[41m",
-  "Bg-Green": "\x1b[42m",
-  "Bg-Yellow": "\x1b[43m",
-  "Bg-Blue": "\x1b[44m",
-  "Bg-Magenta": "\x1b[45m",
-  "Bg-Cyan": "\x1b[46m",
-  "Bg-White": "\x1b[47m",
+	'Bg-Black': '\x1b[40m',
+	'Bg-Red': '\x1b[41m',
+	'Bg-Green': '\x1b[42m',
+	'Bg-Yellow': '\x1b[43m',
+	'Bg-Blue': '\x1b[44m',
+	'Bg-Magenta': '\x1b[45m',
+	'Bg-Cyan': '\x1b[46m',
+	'Bg-White': '\x1b[47m'
 };
 
 const hex2code = (hex, fb) => {
-  if (hex[0] == "#") hex = hex.substr(1, hex.length);
-  let r = parseInt(hex.slice(0, 2), 16),
-    g = parseInt(hex.slice(2, 2), 16),
-    b = parseInt(hex.slice(4, 2), 16);
-  return `\x1b[${fb.toUpperCase() == "B" ? 4 : 3}8;2;${r};${g};${b}m`;
+	if (hex[0] == '#') hex = hex.substr(1, hex.length);
+	let r = parseInt(hex.slice(0, 2), 16),
+		g = parseInt(hex.slice(2, 2), 16),
+		b = parseInt(hex.slice(4, 2), 16);
+	return `\x1b[${fb.toUpperCase() == 'B' ? 4 : 3}8;2;${r};${g};${b}m`;
 };
 
 module.exports = {
-  colors,
-  c: (strings, ...values) => {
-    let text = String.raw({ raw: strings }, ...values);
-    for (let color in colors) {
-      text = text.replace(
-        new RegExp(`\\$\\[${color}\\]`, "gim"),
-        colors[color]
-      );
-    }
-    return text
-      .replace(/\$\[([^]+)\]/g, (_, c) => colors[c] || "")
-      .replace(/@\[(f|b)g\-#(\w{6})\]/gi, (_c, a, b) => hex2code(b, a));
-  },
+	colors,
+	c: (strings, ...values) => {
+		let text = String.raw({ raw: strings }, ...values);
+		for (let color in colors) {
+			text = text.replace(
+				new RegExp(`\\$\\[${color}\\]`, 'gim'),
+				colors[color]
+			);
+		}
+		return text
+			.replace(/\$\[([^]+)\]/g, (_, c) => colors[c] || '')
+			.replace(/@\[(f|b)g\-#(\w{6})\]/gi, (_c, a, b) => hex2code(b, a));
+	}
 };
--- scripts/run-all.js
+++ scripts/run-all.js
@@ -1,109 +1,104 @@
-const scripts = require("../package.json").scripts;
-const spawnCmd = require("spawn-command");
-const { c } = require("./console-colors");
+const scripts = require('../package.json').scripts;
+const spawnCmd = require('spawn-command');
+const { c } = require('./console-colors');
 const psTree = require('ps-tree');
 
 const killTree = (pid, signal, callback) => {
-  signal = signal || "SIGKILL";
-  callback = callback || function () {};
-  psTree(pid, (_err, children) => {
-    [pid, ...children.map((p) => p.PID)].forEach((tpid) => {
-      try {
-        process.kill(tpid, signal);
-      } catch (ex) {}
-    });
-    callback();
-  });
+	signal = signal || 'SIGKILL';
+	callback = callback || function () {};
+	psTree(pid, (_err, children) => {
+		[pid, ...children.map(p => p.PID)].forEach(tpid => {
+			try {
+				process.kill(tpid, signal);
+			} catch (ex) {}
+		});
+		callback();
+	});
 };
 
-const isFlag = arg => arg.startsWith("-");
+const isFlag = arg => arg.startsWith('-');
 
 const args = process.argv.slice(2).filter(arg => !isFlag(arg));
 
 const logs = {};
-let selectedLogId = "";
+let selectedLogId = '';
 
 const updateDisplayedLog = () => {
-  console.clear();
-  // Rerendering the selected log
-  process.stdout.write(logs[selectedLogId].log);
-  // Displaying available commands
-  process.stdout.write(
-    c`$[bg-white]$[bold]$[black] Controls: $[reset] $[bold]$[red]q$[reset]: quit${
-    "     "
-    }$[bold]$[green]n$[reset]: next${
-    "     "
-    }$[bold]$[blue]p$[reset]: previous${
-    "     "
-    }$[bold]$[yellow]r$[reset]: refresh${
-    "     "
-    }Current:$[bold]$[cyan]${
-      Object.keys(logs).indexOf(selectedLogId) + 1
-    }$[reset]/${Object.keys(logs).length}(${selectedLogId})`
-  );
+	console.clear();
+	// Rerendering the selected log
+	process.stdout.write(logs[selectedLogId].log);
+	// Displaying available commands
+	process.stdout.write(
+		c`$[bg-white]$[bold]$[black] Controls: $[reset] $[bold]$[red]q$[reset]: quit${'     '}$[bold]$[green]n$[reset]: next${'     '}$[bold]$[blue]p$[reset]: previous${'     '}$[bold]$[yellow]r$[reset]: refresh${'     '}Current:$[bold]$[cyan]${
+			Object.keys(logs).indexOf(selectedLogId) + 1
+		}$[reset]/${Object.keys(logs).length}(${selectedLogId})`
+	);
 };
 
 const childProcesses = {};
 args.forEach(arg => {
-  // Preparing a regex pattern for the wildcard pattern
-  const wildcardPos = arg.indexOf("*");
-  const wildCardPrefix = arg.slice(0, wildcardPos),
-    wildCardSuffix = arg.slice(wildcardPos + 1);
-  const ptr = new RegExp(`${wildCardPrefix}(.*?)${wildCardSuffix}`);
+	// Preparing a regex pattern for the wildcard pattern
+	const wildcardPos = arg.indexOf('*');
+	const wildCardPrefix = arg.slice(0, wildcardPos),
+		wildCardSuffix = arg.slice(wildcardPos + 1);
+	const ptr = new RegExp(`${wildCardPrefix}(.*?)${wildCardSuffix}`);
 
-  for (let scriptName in scripts) {
-    // ignore scripts that does not match the wildcard pattern
-    if (!ptr.test(scriptName)) continue;
-    const script = scripts[scriptName];
-    const child = spawnCmd(script);
-    logs[scriptName] = { log: "" };
-    child.stdout.on("data", data => {
-      logs[scriptName].log += data;
-      if (scriptName === selectedLogId) updateDisplayedLog();
-    });
-    child.on("exit", () => {
-      process.stdout.write(c`\n$[white][$[yellow]info$[white]]$[cyan] Script '${scriptName}' has exited.`);
-    });
-    childProcesses[child.pid] = child;
-    selectedLogId = scriptName;
-    updateDisplayedLog();
-  }
+	for (let scriptName in scripts) {
+		// ignore scripts that does not match the wildcard pattern
+		if (!ptr.test(scriptName)) continue;
+		const script = scripts[scriptName];
+		const child = spawnCmd(script);
+		logs[scriptName] = { log: '' };
+		child.stdout.on('data', data => {
+			logs[scriptName].log += data;
+			if (scriptName === selectedLogId) updateDisplayedLog();
+		});
+		child.on('exit', () => {
+			process.stdout.write(
+				c`\n$[white][$[yellow]info$[white]]$[cyan] Script '${scriptName}' has exited.`
+			);
+		});
+		childProcesses[child.pid] = child;
+		selectedLogId = scriptName;
+		updateDisplayedLog();
+	}
 });
 
 process.stdin.setRawMode(true);
-process.stdin.setEncoding("utf8");
+process.stdin.setEncoding('utf8');
 
-process.stdin.on("data", key => {
-  let logIdx = Object.keys(logs).indexOf(selectedLogId);
+process.stdin.on('data', key => {
+	let logIdx = Object.keys(logs).indexOf(selectedLogId);
 
-  // Quitting
-  if (key === "\u0003" || key === "q")
-  {
-    // Killing all of the childprocesses
-    for(let child in childProcesses){
-      killTree(child, 0, ()=>{
-        delete childProcesses[child];
-      });
-    }
-    // Waiting for all child processes to terminate
-    setInterval(()=>{
-      if(Object.keys(childProcesses).length === 0) {
-        process.stdout.write(c`\n$[white][$[yellow]info$[white]]$[cyan] All children have been terminated, exiting.....$[reset]`);
-        process.exit();
-      }
-    },100);
-  }
-  // Log of the next command
-  else if (key === "n") {
-    logIdx = Math.min(Object.keys(logs).length - 1, logIdx + 1);
-  }
-  // Log of the previous command
-  else if (key === "p") {
-    logIdx = Math.max(0, logIdx - 1);
-  }
-  // Referesh the current command log
-  else if (key === "r") updateDisplayedLog();
+	// Quitting
+	if (key === '\u0003' || key === 'q') {
+		// Killing all of the childprocesses
+		for (let child in childProcesses) {
+			killTree(child, 0, () => {
+				delete childProcesses[child];
+			});
+		}
+		// Waiting for all child processes to terminate
+		setInterval(() => {
+			if (Object.keys(childProcesses).length === 0) {
+				process.stdout.write(
+					c`\n$[white][$[yellow]info$[white]]$[cyan] All children have been terminated, exiting.....$[reset]`
+				);
+				process.exit();
+			}
+		}, 100);
+	}
+	// Log of the next command
+	else if (key === 'n') {
+		logIdx = Math.min(Object.keys(logs).length - 1, logIdx + 1);
+	}
+	// Log of the previous command
+	else if (key === 'p') {
+		logIdx = Math.max(0, logIdx - 1);
+	}
+	// Referesh the current command log
+	else if (key === 'r') updateDisplayedLog();
 
-  selectedLogId = Object.keys(logs)[logIdx];
-  updateDisplayedLog();
+	selectedLogId = Object.keys(logs)[logIdx];
+	updateDisplayedLog();
 });
