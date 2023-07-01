3e2ea7b56ea678f8d804d3149a4676e094e79394
mohanadft <mohanadfteha@gmail.com>
2023-07-01 19:05:35 +0300

    :sparkles: Setup typeorm to connect with postgres

--- server/apps/auth/src/auth.module.ts
+++ server/apps/auth/src/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
@@ -9,6 +10,14 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: './.env',
     }),
+  TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get("POSTGRES_URI"),
+        synchronize: true,
+      }) 
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService],
